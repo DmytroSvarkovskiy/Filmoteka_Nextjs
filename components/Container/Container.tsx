c5b73ce0bb8c23c30833a6489ff7c6397dc7daeb
DmytroSvarkovskiy <svarkovskii722@gmail.com>
2023-07-13 20:57:01 +0300

    update

--- components/Container/Container.tsx
+++ components/Container/Container.tsx
@@ -1,5 +1,6 @@
 import { ReactNode } from 'react';
 import styled from './styled.module.scss';
+
 export const Container = ({ children }: { children: ReactNode }) => {
   return <div className={styled.container}>{children}</div>;
 };
--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,12 +1,16 @@
 import { Container } from '@/components';
 import styled from './styled.module.scss';
-
+import { GiFilmSpool } from 'react-icons/gi';
 import React from 'react';
 
 export const Header = () => {
   return (
     <header className={styled.header}>
-      <Container>HEADER</Container>
+      <div className={styled.title__wrap}>
+        <GiFilmSpool />
+        <h1>Filmoteka</h1>
+      </div>
+      <input />
     </header>
   );
 };
