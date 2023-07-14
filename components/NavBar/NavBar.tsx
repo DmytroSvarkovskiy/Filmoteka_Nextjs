94e1f03d59c22d1b6df41119d1fbf29c36974782
DmytroSvarkovskiy <svarkovskii722@gmail.com>
2023-07-14 14:15:20 +0300

    -

--- components/NavBar/NavBar.tsx
+++ components/NavBar/NavBar.tsx
@@ -5,17 +5,27 @@ import styled from './styled.module.scss';
 import { usePathname } from 'next/navigation';
 import { RxHamburgerMenu } from 'react-icons/rx';
 import { IoMdClose } from 'react-icons/io';
+
 export const NavBar = () => {
   const path = usePathname();
   const [visible, setVisible] = useState(false);
+
   return (
     <div className={`${styled.navBarContainer} ${visible ? styled.visible : ''}`}>
       <nav className={`${styled.nav} ${visible ? styled.visible : ''}`}>
         <div className={styled.linkWrap}>
-          <Link className={path === '/' ? styled.active : ''} href="/">
+          <Link
+            onClick={() => setVisible(false)}
+            className={path === '/' ? styled.active : ''}
+            href="/"
+          >
             Home
           </Link>
-          <Link className={path === '/library' ? styled.active : ''} href="library">
+          <Link
+            onClick={() => setVisible(false)}
+            className={path === '/library' ? styled.active : ''}
+            href="library"
+          >
             Library
           </Link>
         </div>
