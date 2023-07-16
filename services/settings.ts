3495348001b97a2954f8e1f86a4ea16ff02cf68d
DmytroSvarkovskiy <svarkovskii722@gmail.com>
2023-07-16 09:39:10 +0300

    -

---
+++ services/settings.ts
@@ -0,0 +1,8 @@
+import axios from 'axios';
+const token =
+  'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3YmZlYjMzMzI0ZjcyNTc0MTM2ZDFjZDE0YWU3NjliNSIsInN1YiI6IjYzNWMxMWVmZmQ2MzAwMDA3ZTNmZjZkYiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.TnhuAxfoYoPBod_5WgbIPI3g61qdTUOJbAkoqlv9hFY';
+
+export const apiInstance = axios.create({
+  baseURL: 'https://api.themoviedb.org/3',
+  headers: { common: { Authorization: `Bearer ${token}` }, Accept: 'application/json' },
+});
